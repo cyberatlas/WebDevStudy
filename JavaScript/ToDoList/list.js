8908ec50182e2c7f8c7eb1b69173cee2d95d605b
Alex <ruski550@gmail.com>
2018-03-12 05:30:28 -0500

    Added the delete function to the Todo list, made it nicer and cleaner over all

--- JavaScript/ToDoList/list.js
+++ JavaScript/ToDoList/list.js
@@ -6,16 +6,39 @@ var input = prompt("What would you like to do?");
 while (input !== "quit"){
   //handle input
   if (input === "list"){
-    console.log(todos);
+    listTodos();
   }
   else if (input === "new"){
-    //Ask the user for a new todo
-    var newTodo = prompt("Enter new todo");
-    //add it to the todos array
-    todos.push(newTodo);
+    addTodo();
     }
-
+  else if (input === "delete"){
+    deleteTodo();
+  }
   //ask again for new input
   input = prompt("What would you like to do?");
 }
-console.log("OK, YOU QUIT THE APP")
+console.log("OK, YOU QUIT THE APP");
+
+function listTodos(){
+  console.log("*****");
+  todos.forEach(function(todo, index){
+  console.log(index + ": " + todo);
+  });
+  console.log("*****");
+}
+
+function addTodo(){
+  //Ask the user for a new todo
+  var newTodo = prompt("Enter new todo");
+  //add it to the todos array
+  todos.push(newTodo);
+  console.log(newTodo + " added to list")
+}
+
+function deleteTodo(){
+  //ask for index of item to Delete
+  //delete that todo
+  var index = prompt("Enter the index of a todo to delete");
+  todos.splice(index, 1);
+  console.log("Deleted todo")
+}
